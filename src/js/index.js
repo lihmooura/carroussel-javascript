5179a449dad223b53a9818bf8aaf40af1ed6afce
Elise Melo <elisemelo@Elises-MacBook-Air.local>
2022-05-08 18:30:34 -0300

    fixing layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,27 @@
-import carousel from '../components/carousel'
-main()
+import carousel from '../components/carousel';
+
+fetchCourses()
 
 var AllCourses
 
+function fetchCourses() {
+  fetch('http://localhost:4401/courses')
+.then(response => response.json())
+.then((data) => {
+  AllCourses = data
+  renderCourses()
+});
+}
+
 function renderCourses() {
-    console.log(AllCourses)
     AllCourses.map((elem) => { 
       let carouselList = document.getElementById("carousel-scroll");
       let htmlInsert = `
-      <carousel-component 
+      <carousel-component
         title="${elem.title}"
         time="${elem.time}"   
       />`
 
       carouselList.insertAdjacentHTML('beforeend', htmlInsert);  
     })
-}
-
-function fetchCourses() {
-    fetch('http://localhost:4401/courses')
-  .then(response => response.json())
-  .then((data) => {
-    AllCourses = data
-    renderCourses(AllCourses)
-  });
-}
-
-// $('.scrollto').click(function() {
-//   $('#container').animate( { scrollLeft: '-=500' }, 1000, 'easeOutQuad' );
-// });
-
-function main() {
-    fetchCourses()
 }
